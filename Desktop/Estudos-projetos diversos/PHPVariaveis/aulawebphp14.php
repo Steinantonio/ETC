a7b47ba987163ddd1b64fca1dd414922baeeeab5
antonio.junior <antonio.junior@ntconsult.com.br>
2018-10-29 16:09:22 -0300

    variosobjetos

---
+++ Desktop/Estudos-projetos diversos/PHPVariaveis/aulawebphp14.php
@@ -0,0 +1,26 @@
+﻿
+<?php
+session_name ("alomundo");
+session_start();
+?>
+
+<?php
+/*$arquivo=“contador.txt”;
+$abrir = fopen($arquivo,"r");
+$total = fread($abrir, filesize($arquivo)); 
+fclose($abrir);
+$total = $total + 1; 
+$abrir = fopen($arquivo, "w");
+$salva=fwrite($abrir, $total );
+fclose($abrir);
+echo $total
+*/
+$cont = $_SESSION["cont"]; 
+$_SESSION["cont"]++ ;
+echo $cont;
+
+?>
+
+
+</body>
+</html>
